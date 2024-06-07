0779d9f5a8bdb7be9e6033699e26cb9d59f5a821
mohamadsolouki <mohamad.solouki@gmail.com>
2024-06-07 11:18:51 +0330

    test batch processing

--- batch-processing.py
+++ batch-processing.py
@@ -1,11 +1,16 @@
 import requests
+import os
 
 url = 'http://192.3.45.141:5000//predict'  # Replace with your server URL
-files = [
-    ('files[]', ('1163.jpg', open('data/images/1163.jpg', 'rb'), 'image/jpeg')),
-    ('files[]', ('1164.jpg', open('data/images/1164.jpg', 'rb'), 'image/jpeg')),
-    ('files[]', ('1165.jpg', open('data/images/1165.jpg', 'rb'), 'image/jpeg'))
-]
+test_images_dir = 'test-images'
+files = []
+
+# Loop through all files in the test-images directory
+for filename in os.listdir(test_images_dir):
+    # Only add files with .jpg extension
+    if filename.endswith('.jpg'):
+        file_path = os.path.join(test_images_dir, filename)
+        files.append(('files[]', (filename, open(file_path, 'rb'), 'image/jpeg')))
 
 response = requests.post(url, files=files)
 predictions = response.json()
